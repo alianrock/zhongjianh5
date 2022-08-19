90162438e8c642474376cef9bd42cb3e865b3942
alianrock <654849917@qq.com>
2022-08-19 23:26:27 +0800

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const request = require("request")
 const { init: initDB, Counter } = require("./db");
 
 const logger = morgan("tiny");
